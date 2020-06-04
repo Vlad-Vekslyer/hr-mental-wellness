c73f6daf7bb49e02a3936dbebb3c4491edf1ca9a
unknown <vladi00345@gmail.com>
2020-06-03 22:51:29 -0700

    Added explore and contact section to the footer along with styling

--- footer.php
+++ footer.php
@@ -1,9 +1,22 @@
 <footer>
   <div class="container">
-    <div>
-      <h4>Contact Us</h4>
-      <p><?= get_theme_mod('address'); ?></p>
-      <p><?= get_theme_mod('phone'); ?></p>
+    <div class="top">
+      <div class="explore">
+        <h4>Explore</h4>
+        <?php wp_nav_menu(array(
+          'menu_class' => 'footer-menu',
+          'container' => ''
+        )); ?>
+      </div>
+      <div class="contact">
+        <h4>Contact Us</h4>
+        <p><img src="<?= get_stylesheet_directory_uri() ?>/assets/icons/map-marker-alt-solid.svg"><?= get_theme_mod('address'); ?></p>
+        <p><img src="<?= get_stylesheet_directory_uri() ?>/assets/icons/phone-solid.svg"><?= get_theme_mod('phone'); ?></p>
+        <p>
+          <img src="<?= get_stylesheet_directory_uri() ?>/assets/icons/envelope-regular.svg">
+          <a href="#">Email</a>
+        </p>
+      </div>
     </div>
     <div class="copyrigh-wrapper">
   	    <?php bloginfo('name'); ?> © 2020
