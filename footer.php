dada2e24c950310c6d50793db9edba6c1522e3e2
unknown <vladi00345@gmail.com>
2020-06-04 00:07:13 -0700

    Added book section to the footer and aligned modals + testimonials

--- footer.php
+++ footer.php
@@ -1,12 +1,16 @@
 <footer>
   <div class="container">
     <div class="top">
-      <div class="explore">
-        <h4>Explore</h4>
-        <?php wp_nav_menu(array(
-          'menu_class' => 'footer-menu',
-          'container' => ''
-        )); ?>
+      <div class="book">
+        <h4>Mental Wellness: A Spiritual Journey</h4>
+        <p>In this groundbreaking book, Dr. El-Rayes presents a program of systematic approach to help you heal from depression, anxiety, and addiction.</p>
+        <a href="#">Learn More</a>
+      </div>
+      <div class="newsletter">
+        <h4>Stay Connected</h4>
+        <p>Receive updates about the latest blog posts, webinars and events from HR Mental Wellness</p>
+        <input type="text" name="email" placeholder="Email Address">
+        <button>Subscribe</button>
       </div>
       <div class="contact">
         <h4>Contact Us</h4>
@@ -17,11 +21,12 @@
           <a href="#">Email</a>
         </p>
       </div>
-      <div class="newsletter">
-        <h4>Stay Connected</h4>
-        <p>Receive updates about the latest blog posts, webinars and events from HR Mental Wellness</p>
-        <input type="text" name="email" placeholder="Email Address">
-        <button>Subscribe</button>
+      <div class="explore">
+        <h4>Explore</h4>
+        <?php wp_nav_menu(array(
+          'menu_class' => 'footer-menu',
+          'container' => ''
+        )); ?>
       </div>
     </div>
     <div class="copyrigh-wrapper">
