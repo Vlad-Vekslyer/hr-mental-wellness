8ef00a4e2411d7d855d9976d89c91b5460d8f6d2
unknown <vladi00345@gmail.com>
2020-06-08 18:46:19 -0700

    Added the related pages section to the service template

--- template-service.php
+++ template-service.php
@@ -19,7 +19,17 @@ get_header(); ?>
     <h4>Interested?</h4>
     <p>Call <?= get_theme_mod('phone'); ?> or <a href="#">Subscribe</a> via our registration form.</p>
   </div><!-- .content_holder -->
-</div>
+  <div id="bottom-bar">
+    <?php
+      $tags = wp_get_post_tags($post->ID);
+      if($tags) {
+        $query = apply_filters('tag_query', $tags, $post->ID);
+        if($query->have_posts())
+          do_action('related_pages', $query);
+      }
+    ?>
+  </div><!-- bottom-bar -->
+</div><!-- container -->
 <script src="<?= get_stylesheet_directory_uri() ?>/assets/js/service.js"></script>
 
 <?php get_footer(); ?>
