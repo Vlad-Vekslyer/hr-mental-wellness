8208f52c1211011e5be2ca8fe57e390075277a30
unknown <vladi00345@gmail.com>
2020-06-24 16:15:56 -0700

    Fixed bug that caused sub-menu links not to work on the mobile nav bar. Fixed an issue that caused the sub-menu not to align properly

--- assets/js/mobile-nav.js
+++ assets/js/mobile-nav.js
@@ -7,15 +7,15 @@ hamburgerBtn.addEventListener('click', () => mobileNav.classList.add('show'));
 closeBtn.addEventListener('click', () => mobileNav.classList.remove('show'));
 
 // Toggle sub-menu visibility inside of the mobile nav menu
-const nestedLinks = document.querySelectorAll('.mobile-menu > .menu-item-has-children');
+const nestedLinks = document.querySelectorAll('.mobile-menu > .menu-item-has-children > a');
 
 for(let i = 0; i < nestedLinks.length; i++) {
   nestedLinks.item(i).addEventListener('click', function(event) {
     event.preventDefault();
-    const children = Array.from(this.children);
+    const siblings = Array.from(this.parentElement.children);
     const currentSubMenu = document.querySelector('.sub-menu.show');
     if(currentSubMenu) currentSubMenu.classList.remove('show');
-    const subMenu = children.find(child => child.nodeName === 'UL' && child.classList.contains('sub-menu'));
+    const subMenu = siblings.find(sibling => sibling.nodeName === 'UL' && sibling.classList.contains('sub-menu'));
     subMenu.classList.add('show');
   })
 }
