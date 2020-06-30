eca77e8cc0942c5862a1f621653e2f972800d784
Vlad Veksler <vladi00345@gmail.com>
2020-06-30 11:25:07 -0700

    Refactored logic that prints post_preview markup into an action hook

--- category.php
+++ category.php
@@ -10,32 +10,7 @@ get_header(); ?>
         'category_name' => $cat_slug
       );
       $query = new WP_Query($args);
-      while($query->have_posts()) {
-        $query->the_post();
-        $categories = get_the_category();
-        ?>
-
-        <div class="post-preview">
-          <h2><?= the_title(); ?></h2>
-          <h5><?= the_date("M d, Y"); ?></h5>
-          <p><?= substr(get_the_content(), 0, 300); ?>...</p>
-          <div class="bottom">
-            <a href=<?= the_permalink(); ?>>Read More</a>
-            <ul class="categories">
-              <?php
-                foreach($categories as $index => $category) {
-                  $name = $category->to_array()['name'];
-                  $suffix = $index !== sizeof($categories) - 1 ? "&nbsp•&nbsp" : '';
-                  echo "<li><h5>$name$suffix</h5></li>";
-                }
-              ?>
-            </ul>
-          </div>
-        </div>
-
-        <?php
-      }
-      wp_reset_query();
+      do_action('post_preview', $query);
     ?>
   </div><!-- .content_holder -->
   <aside class="two-column">
--- functions.php
+++ functions.php
@@ -94,6 +94,36 @@ function print_related_pages(WP_Query $query) {
 }
 add_action('related_pages', 'print_related_pages');
 
+function print_post_preview(WP_Query $query) {
+  while($query->have_posts()) {
+    $query->the_post();
+    $categories = get_the_category();
+    ?>
+
+    <div class="post-preview">
+      <h2><?= the_title(); ?></h2>
+      <h5><?= the_date("M d, Y"); ?></h5>
+      <p><?= substr(get_the_content(), 0, 300); ?>...</p>
+      <div class="bottom">
+        <a href=<?= the_permalink(); ?>>Read More</a>
+        <ul class="categories">
+          <?php
+            foreach($categories as $index => $category) {
+              $name = $category->to_array()['name'];
+              $suffix = $index !== sizeof($categories) - 1 ? "&nbsp•&nbsp" : '';
+              echo "<li><h5>$name$suffix</h5></li>";
+            }
+          ?>
+        </ul>
+      </div>
+    </div>
+
+    <?php
+  }
+  wp_reset_query();
+}
+add_action('post_preview', 'print_post_preview');
+
 // Import and register custom customization options
 require get_stylesheet_directory() . '/inc/customizer.php';
 // Import and register custom widgets
